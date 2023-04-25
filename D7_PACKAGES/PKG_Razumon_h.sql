c1a15458e38b096d87b5f500bb5779786ed607f2
IKSAM <sam.rotthier@student.kdg.be>
2023-04-25 21:33:03 +0200

    verderwerken aan M4

---
+++ D7_PACKAGES/PKG_Razumon_h.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE PACKAGE PKG_Razumon
+AS
+
+
+    PROCEDURE empty_tables;
+
+    PROCEDURE add_player(
+    p_name player.name%TYPE,
+    p_gender player.gender%TYPE,
+    p_level player.level%TYPE,
+    p_timeplayed player.timeplayed%TYPE,
+    p_homeaddress player.homeaddress%TYPE,
+    p_startdate player.startdate%TYPE,
+    p_lastlogindate player.lastlogindate%TYPE
+    );
+
+    PROCEDURE add_guild(
+    p_guildname guild.guildname%TYPE,
+    p_guildskill guild.guildskill%TYPE,
+    p_madeon guild.madeon%TYPE,
+    p_level guild.level%TYPE,
+    p_madeby guild.madeby%TYPE
+    );
+
+  PROCEDURE add_team(
+    p_teamname player.teamname%TYPE,
+    p_timeplayedwithteam player.timeplayedwithteam%TYPE
+    );
+
+    PROCEDURE add_monster(
+    p_monstername monster.monstername%TYPE,
+    p_health monster.health%TYPE,
+    p_level monster.level%TYPE,
+    p_canevolve monster.canevolve%TYPE
+    );
+
